f004a5ac7ae634ff7d8f0f7200f8bb91ca3627fe
Jonas Mueller Gastell <jonasmg@stanford.edu>
2017-11-17 09:30:28 -0800

    DEPLOY

---
+++ conformity/models.py
@@ -0,0 +1,211 @@
+from __future__ import division
+
+from otree.api import (
+    models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
+    Currency as c, currency_range
+)
+
+
+from django.conf import settings
+
+import os
+import random	 
+import numpy as np
+
+
+
+from math import ceil
+
+author = 'Jonas Mueller-Gastell'
+
+doc = """
+This is the main part of the experiment.
+"""
+
+
+
+class Constants(BaseConstants):
+### Randomness	
+	seeder = os.urandom(4)
+	random.seed(seeder)
+
+### Charities:
+	charities = ["Amarillo","Austin","El Paso","Fort Worth","Houston","Irving","Lubbock","Midland","San Antonio","Tyler"] 
+
+### Base constants
+	name_in_url = 'Experiment'
+	players_per_group = None
+	num_rounds = len(charities)
+
+### TREATMENT:
+	# public = settings.SESSION_CONFIGS[0].get('public')
+	# extraDonationTreat = settings.SESSION_CONFIGS[0].get('extraDonationTreat')
+	# noFeedback = settings.SESSION_CONFIGS[0].get('noFeedback')
+	# public = False
+	# extraDonationTreat = True
+	# noFeedback = False
+	numberunderstandingquestions = 11
+	accuracy = 1
+
+### Money
+	money = [c(3.72),c(5.72),c(6.72),c(7.67),c(8.57),c(9.42),c(10.22),c(10.97),c(11.67),c(12.32),c(12.92),c(13.47),c(13.97),c(14.42),c(14.82),c(15.17),c(15.47),c(15.72),c(15.92),c(16.07),c(16.17),c(16.26),c(16.34),c(16.41),c(16.47),c(16.52),c(16.56),c(16.59),c(16.61),c(16.62),c(16.62),c(16.63),c(16.63),c(16.63),c(16.64),c(16.64) ]
+	showup = c(20)
+	bonus = c(5)
+	bonusForConfidence = 3
+	confidenceBonus =c(bonusForConfidence)
+	extraDonationNum = 5	
+	extraDonation = c(extraDonationNum)
+
+	r = np.linspace(1,bonusForConfidence,9)
+	r2 = np.linspace(0,extraDonationNum,13)
+	right_side_amounts = [c(0.05), c(0.10), c(0.25), c(0.50), c(0.75)] + [c(round(float(k),2)) for k in r]
+	right_side_amounts_charity = list(reversed([c(float(k)) for k in r2]))
+
+
+### TIMER:
+	timerPractice = settings.SESSION_CONFIGS[0].get('practiceWaiting')
+
+
+
+
+class Subsession(BaseSubsession):
+
+#### PAYOFF RANDOMISATION:
+	def creating_session(self):
+		self.session.vars["NumberOfPlayers"] = len(self.get_players())
+		selectedPlayer = random.randint(1,self.session.vars.get("NumberOfPlayers"))	
+		self.session.vars["selectedPlayer"] = selectedPlayer
+
+		self.session.vars["selectedRound"] = random.randint(1, Constants.num_rounds)
+
+		selectedIncentive = random.randint(1,2)
+		self.session.vars["selectedIncentive"] = selectedIncentive
+
+		# for player in self.get_players():
+		# 	selectedExtra = random.random()
+		# 	if selectedExtra > 0.5:
+		# 		player.participant.vars["DonateFirst"] = True
+		# 		player.participant.vars["DonateSecond"] = False
+		# 	else:
+		# 		player.participant.vars["DonateFirst"] = False
+		# 		player.participant.vars["DonateSecond"] = True
+
+
+
+class Group(BaseGroup):
+	pass
+	#medianCommitment = models.PositiveIntegerField()
+
+
+
+
+
+class Player(BasePlayer):
+####### QUIZZ 
+	truefalse0 = models.PositiveIntegerField(verbose_name="Question 1: How many of your waiting-time decisions, at most, will count for payment?")
+	truefalse1 = models.PositiveIntegerField(verbose_name="Question 2: How many of your bonus-payment decisions, at most, will count for payment?")
+	truefalse2 = models.BooleanField(choices=[[1, 'Yes'],[0, 'No']],widget=widgets.RadioSelect(),verbose_name=("Question 3: Before you make your decisions in Rounds 2 through" + " " +str(Constants.num_rounds) +", will you know some information about the decisions made by others in the previous round?"))
+	truefalse3 = models.BooleanField(choices=[[1, 'Yes'],[0, 'No']],widget=widgets.RadioSelect(),verbose_name=("Question 4: Before making their decisions in Rounds 2 through" + " " +str(Constants.num_rounds) +", will others know your decision in particular in previous rounds?"))
+	truefalse4 = models.BooleanField(choices=[[1, 'Yes'],[0, 'No']],widget=widgets.RadioSelect(),verbose_name="Question 5: After all decisions are made, will others in this room learn your decision from the selected round?")
+	truefalse5 = models.BooleanField(choices=[[1, 'Yes'],[0, 'No']],widget=widgets.RadioSelect(),verbose_name="Question 6: After all decisions are made, will others in this room learn your decisions from the rounds that were not selected?")
+	truefalse6 = models.PositiveIntegerField(choices=[[0, 'The amount of time you chose to stay in the selected round.'],[1, 'The amount of time others chose to stay in the selected round'],[2, 'None of the above']],widget=widgets.RadioSelect(),verbose_name="Question 7: If you ARE the selected participant, your earnings for Make-A-Wish Foundation will depend on:")
+	truefalse7 = models.PositiveIntegerField(choices=[[0, 'The amount of time you chose to stay in the selected round.'],[1, 'The amount of time others chose to stay in the selected round'],[2, 'No additional time']],widget=widgets.RadioSelect(),verbose_name="Question 8: If you ARE the selected participant, you will have to wait:")
+	truefalse8 = models.PositiveIntegerField(choices=[[0, 'The amount of time you chose to stay in the selected round.'],[1, 'The amount of time others chose to stay in the selected round'],[2, 'None of the above']],widget=widgets.RadioSelect(),verbose_name="Question 9: If you ARE NOT the selected participant, your earnings for Make-A-Wish Foundation will depend on:")
+	truefalse9 = models.PositiveIntegerField(choices=[[0, 'The amount of time you chose to stay in the selected round.'],[1, 'The amount of time others chose to stay in the selected round'],[2, 'No additional time']],widget=widgets.RadioSelect(),verbose_name="Question 10: If you ARE NOT the selected participant, you will have to wait:")
+	truefalse10 = models.PositiveIntegerField(verbose_name= "Question 11: Look at the decision table below. Please enter how much money (in whole dollars) the participant will be paid if the true median is 12 minutes and row number 5 is implemented by the computer and this question is chosen for payment.")
+	truefalse11 = models.PositiveIntegerField(verbose_name= "Question 12: Look at the decision table below. Please enter how much money (in whole dollars) the participant will be paid if the true median is 16 minutes and row number 10 is implemented by the computer and this question is chosen for payment.")
+
+######## ACTUAL DATA COLLECTED
+	commitment = models.PositiveIntegerField(min=0,max=35)
+
+	belief = models.PositiveIntegerField(min=0,max=35)
+
+	confidence = models.CurrencyField()
+
+	medianCommitment = models.PositiveIntegerField()
+	timeMinutes = models.PositiveIntegerField()
+
+	DonationDecision = models.CurrencyField()
+
+	def calcmedian(self):
+		commits = []
+		for player in self.get_others_in_group():
+			commits.append(player.commitment)
+		commits = sorted(commits)
+		self.medianCommitment = commits[ceil(self.session.vars.get("NumberOfPlayers")/2)-1]
+		return self.medianCommitment
+
+	def commitWait(self):
+		roundN = str(self.session.vars.get("selectedRound")) 
+		nameC = "commitment" + roundN 
+		self.timeMinutes = self.participant.vars.get(nameC)
+		return self.timeMinutes
+
+######## Survey
+	impactBelief   = models.PositiveIntegerField(verbose_name="My choices about my own waiting times were impacted by what I THOUGHT other participants chose.", choices=[[1,"Strongly Disagree"],[2, "Disagree"],[3, "Neutral"],[4, "Agree"],[5,"Strongly Agree"]], widget=widgets.RadioSelectHorizontal())
+	impactKnow     = models.PositiveIntegerField(verbose_name="My choices about my own waiting times were impacted by how much I KNEW other participants chose.", choices=[[1,"Strongly Disagree"],[2, "Disagree"],[3, "Neutral"],[4, "Agree"],[5,"Strongly Agree"]], widget=widgets.RadioSelectHorizontal())
+	impactLocation = models.PositiveIntegerField(verbose_name="My choices about my own waiting times were impacted by the different locations of the Make-A-Wish Foundation across rounds.", choices=[[1,"Strongly Disagree"],[2, "Disagree"],[3, "Neutral"],[4, "Agree"],[5,"Strongly Agree"]], widget=widgets.RadioSelectHorizontal())
+	impactTexas    = models.PositiveIntegerField(verbose_name="My choices about my own waiting times were impacted by the fact that the Make-A-Wish Foundation locations involved in this study were in Texas, as opposed to some other state in the U.S.", choices=[[1,"Strongly Disagree"],[2, "Disagree"],[3, "Neutral"],[4, "Agree"],[5,"Strongly Agree"]], widget=widgets.RadioSelectHorizontal())
+	impactFair     = models.PositiveIntegerField(verbose_name="My choices about my own waiting times were impacted by what I thought was fair.", choices=[[1,"Strongly Disagree"],[2, "Disagree"],[3, "Neutral"],[4, "Agree"],[5,"Strongly Agree"]], widget=widgets.RadioSelectHorizontal())
+	impactNice     = models.PositiveIntegerField(verbose_name="My choices about my own waiting times were impacted by what I thought was nice.", choices=[[1,"Strongly Disagree"],[2, "Disagree"],[3, "Neutral"],[4, "Agree"],[5,"Strongly Agree"]], widget=widgets.RadioSelectHorizontal())
+	impactGoodLook = models.PositiveIntegerField(verbose_name="My choices about my own waiting times were impacted by what I thought looked good to others.", choices=[[1,"Strongly Disagree"],[2, "Disagree"],[3, "Neutral"],[4, "Agree"],[5,"Strongly Agree"]], widget=widgets.RadioSelectHorizontal())
+	favMakeAWish   = models.PositiveIntegerField(verbose_name="I feel favorably about the Make-A-Wish Foundation." , choices=[[1,"Strongly Disagree"],[2, "Disagree"],[3, "Neutral"],[4, "Agree"],[5,"Strongly Agree"]], widget=widgets.RadioSelectHorizontal())
+
+	studentID = models.CharField(verbose_name="What is your Stanford Student ID Number?")
+	sex = models.PositiveIntegerField(verbose_name="What is your sex?", choices=[[0,"Female"], [1,"Male"], [2, "Decline To State or Other"]], widget=widgets.RadioSelect())
+	age = models.PositiveIntegerField(verbose_name="What is your age (in years)?", min=18,max=30)
+	gradYear = models.PositiveIntegerField(verbose_name="In what year do you expect to graduate?", min=2018, max=2030)
+	school = models.PositiveIntegerField(verbose_name="In what school is your degree program (or expected degree program)?", choices=[[1, "School of Business"],[2, "School of Eath Sciences"], [3,"School of Education"],[4, "School of Engineering"], [5,"School of Humanities & Sciences"], [6,"School of Law"], [7,"School of Medicine"], [8 ,"Unknown"]], widget=widgets.RadioSelect())
+	econMajor= models.BooleanField(verbose_name="Are you an economics major?",choices=[[1, 'Yes'],[0, 'No']],widget=widgets.RadioSelect())
+	GPA = models.FloatField(verbose_name="What is your GPA (on a 4.0 scale)?", min=0,max=4)
+
+
+
+### SET PAYOFF
+	def set_payoffs(self):
+		roundN = str(self.session.vars.get("selectedRound")) 
+		nameB = "belief" + roundN
+		nameM = "median" + roundN
+		nameC = "confidence" + roundN
+
+		beliefOfSelectedRound = self.participant.vars.get(nameB)
+		medianInSelectedRound = self.participant.vars.get(nameM)
+		confidenceOfSelectedRound = self.participant.vars.get(nameC)
+
+		selectedIncentive = self.session.vars.get("selectedIncentive")
+		
+		if selectedIncentive == 1:
+			if beliefOfSelectedRound == medianInSelectedRound:
+				bonus1 = Constants.bonus
+			else:
+				bonus1 = c(0)
+			bonus2 = c(0)
+		elif selectedIncentive == 2:
+			randomVar = random.randint(0,len(Constants.right_side_amounts))
+			if Constants.right_side_amounts[randomVar] <= confidenceOfSelectedRound:
+				if  beliefOfSelectedRound in ([medianInSelectedRound] + [medianInSelectedRound - k for k in range(1,Constants.accuracy+1)] + [medianInSelectedRound + k for k in range(1,Constants.accuracy+1)]):
+					bonus2 = Constants.confidenceBonus
+				else:
+					bonus2 = c(0)
+			else:
+				bonus2 = Constants.right_side_amounts[randomVar]
+			bonus1 = c(0)
+
+		if Constants.extraDonationTreat:
+			randomRow = random.randint(0,len(Constants.right_side_amounts_charity)-1)
+			if Constants.right_side_amounts_charity[randomRow] > self.participant.vars.get("DonationAmount"):
+				bonus3 = Constants.right_side_amounts_charity[randomRow]
+				self.participant.vars["outcomeCharityOwn"] = False
+			else:
+				self.participant.vars["charityDonation"] = Constants.extraDonation
+				bonus3 = c(0)
+				self.participant.vars["outcomeCharityOwn"] = True
+		else:
+			bonus3 = c(0)
+
+		self.payoff = Constants.showup + bonus1 + bonus2 + bonus3
+		return self.payoff
+
+
+
+
---
+++ conformity/views.py
@@ -0,0 +1,287 @@
+from otree.api import Currency as c, currency_range
+from . import models
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+
+### INTRO PAGES
+
+class welcome(Page):
+	def is_displayed(self):
+		return self.round_number == 1
+
+class IRB(Page):
+	def is_displayed(self):
+		return self.round_number == 1
+
+
+
+class donationFirst(Page):
+	def is_displayed(self):
+		return  self.session.config.get('extraDonationTreat') and self.round_number == Constants.num_rounds
+
+class donationFirstDecision(Page):
+	def is_displayed(self):
+		return   self.session.config.get('extraDonationTreat')  and self.round_number == Constants.num_rounds
+
+	form_model = models.Player
+	form_fields = ['DonationDecision']
+
+	def vars_for_template(self):
+		return {"left_side": Constants.extraDonation, "right_side_amounts_charity": Constants.right_side_amounts_charity}
+	
+	def before_next_page(self):
+		self.player.participant.vars["DonationAmount"] = self.player.DonationDecision
+
+
+
+
+class instructions(Page):
+	def is_displayed(self):
+		return self.round_number == 1
+	def vars_for_template(self):
+		return {  "min": Constants.right_side_amounts[0], "public": self.session.config.get('public'), "noFeedback": self.session.config.get('noFeedback'), "max": Constants.right_side_amounts[len(Constants.right_side_amounts)-1], "number": len(Constants.right_side_amounts)}
+
+class quizz(Page):
+	def is_displayed(self):
+		return self.round_number == 1
+	def vars_for_template(self):
+			return { "public": self.session.config.get('public'), "noFeedback": self.session.config.get('noFeedback')}
+
+
+
+	form_model = models.Player
+	def get_form_fields(self):
+		fields_to_show=[]
+		for key in range(Constants.numberunderstandingquestions+1):
+			fields_to_show.append('truefalse{}'.format(key))
+		return fields_to_show
+
+	def vars_for_template(self):
+		return {"one": 1, "two": 2}
+
+	def error_message(self, values):
+		summand = []
+		if values["truefalse0"] != 1:
+			summand = summand + ["Question 1"]
+		if values["truefalse1"] != 1:
+			summand = summand + ["Question 2"]
+		if values["truefalse2"] == self.session.config.get('noFeedback'):
+			summand = summand + ["Question 3"]
+		if values["truefalse3"] != False:
+			summand = summand + ["Question 4"]
+		if values["truefalse4"] != self.session.config.get('public'):
+			summand = summand + ["Question 5"]
+		if values["truefalse5"] != False:
+			summand = summand + ["Question 6"]	
+		if values["truefalse6"] != 0:
+			summand = summand + ["Question 7"]	
+		if values["truefalse7"] != 0:
+			summand = summand + ["Question 8"]
+		if values["truefalse8"] != 2:
+			summand = summand + ["Question 9"]	
+		if values["truefalse9"] != 2:
+			summand = summand + ["Question 10"]	
+		if values["truefalse10"] != 3:
+			summand = summand + ["Question 11"]	
+		if values["truefalse11"] != 2:
+			summand = summand + ["Question 12"]	
+		if len(summand) == 1:	
+			text = "Your answer to the following question is incorrect: " + ", ".join(summand) + "."
+			current = self.participant.vars.get("NumberMistakes")
+			if current:
+				if summand > current:
+					self.participant.vars["NumberMistakes"] = summand
+			else:
+				self.participant.vars["NumberMistakes"] = summand
+			return text	
+		elif len(summand) > 1:	
+			text = "Your answers to the following questions are incorrect: " + ", ".join(summand) + "."
+			current = self.participant.vars.get("NumberMistakes")
+			if current:
+				if summand > current:
+					self.participant.vars["NumberMistakes"] = summand
+			else:
+				self.participant.vars["NumberMistakes"] = summand
+			return text	
+		# summand = 0
+		# if values["truefalse1"] != 1:
+		# 	summand += 1
+		# if values["truefalse2"] != Constants.noFeedback:
+		# 	summand += 1
+		# if values["truefalse3"] != False:
+		# 	summand += 1
+		# if values["truefalse4"] != Constants.public:
+		# 	summand += 1
+		# if values["truefalse5"] != False:
+		# 	summand += 1	
+		# if values["truefalse6"] != 0:
+		# 	summand += 1	
+		# if values["truefalse7"] != 0:
+		# 	summand += 1	
+		# if values["truefalse8"] != 2:
+		# 	summand += 1	
+		# if values["truefalse9"] != 2:
+		# 	summand += 1	
+		# if values["truefalse10"] != 3:
+		# 	summand += 1	
+		# if values["truefalse11"] != 2:
+		# 	summand += 1	
+		# if summand > 1:	
+		# 	return 'Sorry, you got ' + str(summand) + " questions wrong."
+		# 	summand = 0
+		# elif summand == 1:
+		# 	summand = 0
+		# 	return 'Almost there! You just got one question wrong!'
+
+
+class practice(Page):
+	def is_displayed(self):
+		return self.round_number == 1	
+	timeout_seconds = Constants.timerPractice
+	timer_text = "Time left to wait:"
+
+
+
+
+#### MAIN PAGES
+
+class commit(Page):
+	form_model = models.Player 
+	form_fields = ['commitment', 'belief']
+	def vars_for_template(self):
+		return {"charity": Constants.charities[self.round_number-1], "listCommits1": Constants.money[0:12], "listCommits2": Constants.money[12:24], "listCommits3": Constants.money[24:36], "one": 1}
+	def before_next_page(self):
+		nameC = "commitment" + str(self.round_number)
+		nameB = "belief" + str(self.round_number)
+		self.player.participant.vars[nameC] = self.player.commitment
+		self.player.participant.vars[nameB] = self.player.belief
+
+class confidence(Page):
+	form_model = models.Player 
+	form_fields = ['confidence']
+	def vars_for_template(self):
+		return {"one": 1, "left_side": Constants.confidenceBonus, "right_side": Constants.confidenceBonus, "right_side_amounts": Constants.right_side_amounts, 'belief': self.player.belief, "committed": self.player.belief}
+	def before_next_page(self):
+		nameC = "confidence" + str(self.round_number)
+		self.player.participant.vars[nameC] = self.player.confidence
+
+
+class feedback(Page):
+	def is_displayed(self):
+		return self.session.config.get('noFeedback') == False and self.round_number < Constants.num_rounds
+	def vars_for_template(self):
+		return {"median": self.player.calcmedian(),  "one": 1, "money": Constants.money[self.player.calcmedian()-1], "charity":Constants.charities[self.round_number-1]}
+
+
+class feedbackLast(Page):
+	def is_displayed(self):
+		return self.session.config.get('noFeedback') == False and self.round_number == Constants.num_rounds
+	def vars_for_template(self):
+		return {"median": self.player.calcmedian(), "one": 1, "money": Constants.money[self.player.calcmedian()-1], "charity":Constants.charities[self.round_number-1]}
+
+
+class publicWaitingInstructions(Page):
+	def is_displayed(self):
+		return self.round_number == Constants.num_rounds  and self.session.config.get('public') == True
+	def vars_for_template(self):
+#		return {"one": 1, "committed": self.player.participant.vars.get("committedMinutes")*60,"committedMin": self.player.participant.vars.get("committedMinutes"), "money": Constants.money[self.player.timeMinutes -1],"round": self.session.vars.get("selectedRound"), "charity":Constants.charities[self.session.vars.get("selectedRound")-1] }
+		return {"one": 1, "committed": self.player.timeMinutes*60,"committedMin": self.player.timeMinutes, "money": Constants.money[self.player.timeMinutes -1],"round": self.session.vars.get("selectedRound"), "charity":Constants.charities[self.session.vars.get("selectedRound")-1] }
+
+
+class waiting(Page):
+	def is_displayed(self):
+		return self.round_number == Constants.num_rounds and self.player.id_in_group == self.session.vars.get("selectedPlayer")
+	timer_text = "Time left to wait:"	
+	def get_timeout_seconds(self):	
+		return self.player.participant.vars.get("committedMinutes")*60
+	def vars_for_template(self):
+		return {"one": 1, "committed": self.player.participant.vars.get("committedMinutes")*60,"committedMin": self.player.participant.vars.get("committedMinutes"), "money": Constants.money[self.player.timeMinutes -1],"round": self.session.vars.get("selectedRound"), "charity":Constants.charities[self.session.vars.get("selectedRound")-1] }
+	def before_next_page(self):
+		self.player.set_payoffs()
+
+class notwaiting(Page):
+	def is_displayed(self):
+		return self.round_number == Constants.num_rounds and self.player.id_in_group != self.session.vars.get("selectedPlayer")
+	def before_next_page(self):
+		self.player.set_payoffs()
+
+
+
+
+##### SURVEY PAGES
+
+class survey1(Page):
+	def is_displayed(self):
+		return self.round_number == Constants.num_rounds
+
+	form_model = models.Player
+	form_fields = ['impactBelief', 'impactKnow', 'impactLocation', 'impactTexas', 'impactFair', 'impactNice', 'impactGoodLook','favMakeAWish']
+
+
+class survey2(Page):
+	def is_displayed(self):
+		return self.round_number == Constants.num_rounds
+
+	form_model = models.Player
+	form_fields = ['studentID', 'sex', 'age', 'gradYear', 'school', 'econMajor', 'GPA']
+
+
+
+class payment(Page):
+	def is_displayed(self):
+		return self.round_number == Constants.num_rounds
+	def vars_for_template(self):
+		if self.player.payoff > Constants.showup: # + Constants.bonus or self.player.payoff == Constants.showup + Constants.bonus + Constants.confidenceBonus:
+			extra = True
+		else:
+			extra = False
+		# if self.player.payoff == Constants.showup + Constants.confidenceBonus or self.player.payoff == Constants.showup + Constants.bonus + Constants.confidenceBonus:
+		# 	extra2 = True
+		# else:
+		# 	extra2 = False
+		if self.session.vars.get("selectedPlayer") == self.player.id_in_group:
+			waiting = True
+		else:
+			waiting = False
+		return {"payment": self.player.payoff, "extraDonationTreat": self.session.config.get("extraDonationTreat"), "shareBool": self.player.participant.vars.get("outcomeCharityOwn"), "extra": extra, "waiting": waiting, "round": self.session.vars.get("selectedRound"), "charity": Constants.charities[self.session.vars.get("selectedRound")-1], "money": Constants.money[self.player.timeMinutes -1]} 	
+
+
+
+##### WAIT PAGES
+ 
+class quizzWaitPage(WaitPage):
+	def is_displayed(self):
+		return self.round_number == 1
+
+class ResultsWaitPage(WaitPage):
+	def after_all_players_arrive(self):
+		for player in self.group.get_players():
+			player.medianCommitment = player.calcmedian()
+			nameM = "median" +str(self.round_number)
+			player.participant.vars[nameM] = player.medianCommitment
+			if self.round_number == Constants.num_rounds:
+				player.timeMinutes = player.commitWait()
+				player.participant.vars["committedMinutes"] = player.commitWait()
+
+page_sequence = [
+	# welcome,
+	# IRB,
+	instructions,
+	quizz,
+	quizzWaitPage,
+	practice,
+	commit,
+	confidence,
+	ResultsWaitPage,
+	feedback,
+	feedbackLast,
+	survey1,
+	survey2,
+	donationFirst,
+	donationFirstDecision,
+	publicWaitingInstructions,
+	waiting,
+	notwaiting,
+	payment
+]
